15872f29b23bf27d5512ede599a90faca3383e68
Lakshana Priya <lakshanapriyak@gmail.com>
2026-05-10 09:39:15 +0530

    Initial commit - Bus Booking Spring Boot project

---
+++ busbooking/src/main/java/org/example/busbooking/BusbookingApplication.java
@@ -0,0 +1,13 @@
+package org.example.busbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@org.example.busbooking.SpringBootApplication
+public class BusbookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusbookingApplication.class, args);
+    }
+
+}
